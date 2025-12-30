58af95eb1ff99ca90d3578b42ebb7f982cdf3e48
M Farhan Ramadhan <frhn.r3@gmail.com>
2025-12-30 22:34:44 +0700

    fix: vercel configuration fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
@@ -20,8 +21,24 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        /** @var Request $request */
+        $request = $this->app->make(Request::class);
+
+        $appUrl = config('app.url');
+
+        if (empty($appUrl) || str_contains($appUrl, 'localhost')) {
+            $appUrl = $request->getSchemeAndHttpHost();
+        }
+
+        $normalizedRoot = preg_replace('#/api$#', '', rtrim($appUrl ?? '', '/'));
+
+        if (! empty($normalizedRoot)) {
+            config(['app.url' => $normalizedRoot]);
+        }
+
         if ($this->app->environment('production')) {
-        \URL::forceScheme('https');
-    }
+            URL::forceRootUrl(config('app.url'));
+            URL::forceScheme('https');
+        }
     }
 }
